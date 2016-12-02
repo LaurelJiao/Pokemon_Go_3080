58bb094208c49235e1d62e16f09841403751f079
MadLaurel <ruixinjiao@gmail.com>
2016-12-02 17:42:52 +0800

    implement Evovled(), derived Pokemon

--- Pokemon_Go_3080/Pokemon_Go_3080/Model.cs
+++ Pokemon_Go_3080/Pokemon_Go_3080/Model.cs
@@ -1,9 +1,17 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 namespace Pokemon_Go_3080
 {
     public interface IModel { }
@@ -15,6 +23,10 @@ namespace Pokemon_Go_3080
     class Player : IModel
     {
         private Bag Bag;
+        public int Level { private set; get; }
+        public int Exp_Max { private set; get; }
+        public int Exp { private set; get; }
+        public int[,] Position { private set; get; }
         private Player()
         { }
         static private Player instance;
@@ -27,6 +39,12 @@ namespace Pokemon_Go_3080
                 return instance;
             }
         }
+        public int[,] Move()
+        {
+            //....
+            return Position;
+        }
+
     }
     class Bag : IModel
     {
@@ -36,13 +54,18 @@ namespace Pokemon_Go_3080
     {
 
     }
-    class Pokemon : IModel
+    public abstract class Pokemon : IModel
     {
         public string name
         {
             private set; get;
         }
+        public string type
+        {
+            private set;get;
+        }
         // need a field for the image
+        protected BitmapImage Pokemon_image;
         private int Hp_Maximum;
         public int Hp
         {
@@ -59,11 +82,10 @@ namespace Pokemon_Go_3080
 
         public Pokemon()
         {
-
         }
 
-        public void Powerup() { }
-        public void Envolved() { }
+        public abstract void Powerup();
+        public abstract void Evolved();
         public bool Deduct_Health(int deduct_num)
         {
             // Deduct the health, after the operation, return true is Hp <= 0
@@ -75,8 +97,52 @@ namespace Pokemon_Go_3080
             }
             return false;
         }
+        public void PlayerSetName(string name)
+        {
+            this.name = name;
+        }
     }
 
+    class Pichu: Pokemon
+    { // Example
+        private BitmapImage Pichu_Image = new BitmapImage(new Uri("image path"));
+       
+        private string type = "Pichu";
+        public Pichu()
+        {
+        }
+        public override void Powerup()
+        {
+            throw new NotImplementedException();
+        }
+        public override void Evolved()
+        {
+            Pichu_Image.UriSource = new Uri("/Pikachu_image.jpg");
+           
+           // convert from pichu to pikachu obj ??
+
+        }
+        
+    }
+    class Pikachu : Pokemon
+    { // Example
+        private BitmapImage Pikachu_Image = new BitmapImage();
+        private string type = "Pikachu";
+        public Pikachu()
+        {
+        }
+        public override void Powerup()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Evolved()
+        {
+            Pikachu_Image.UriSource = new Uri("/image.jpg");
+            // Pikachu as Raicuh r;         
+        }
+
+    }
     class Battle_Gym : IModel
     {
         public delegate void Callback(Pokemon winner, Pokemon loser);
