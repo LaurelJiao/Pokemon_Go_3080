66f6bd9674a0cbc613081e9c306eb377848c729d
MadLaurel <ruixinjiao@gmail.com>
2016-12-02 16:05:57 +0800

    Add files via upload

---
+++ Pokemon_Go_3080/Pokemon_Go_3080/Model.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemon_Go_3080
+{
+    public interface IModel { }
+
+    class Game_Model : IModel
+    {
+        private Player Player;
+    }
+    class Player : IModel
+    {
+        private Bag Bag;
+        private Player()
+        { }
+        static private Player instance;
+        static public Player Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new Player();
+                return instance;
+            }
+        }
+    }
+    class Bag : IModel
+    {
+        private List<Pokemon> MyPokemons;
+    }
+    class Typing_Game : IModel
+    {
+
+    }
+    class Pokemon : IModel
+    {
+        public string name
+        {
+            private set; get;
+        }
+        // need a field for the image
+        private int Hp_Maximum;
+        public int Hp
+        {
+            private set; get;
+        }
+        public int Cp
+        {
+            private set; get;
+        }
+        public int Damage
+        {
+            private set; get;
+        }
+
+        public Pokemon()
+        {
+
+        }
+
+        public void Powerup() { }
+        public void Envolved() { }
+        public bool Deduct_Health(int deduct_num)
+        {
+            // Deduct the health, after the operation, return true is Hp <= 0
+            Hp -= deduct_num;
+            if (Hp <= 0)
+            {
+                Hp = 0;
+                return true;
+            }
+            return false;
+        }
+    }
+
+    class Battle_Gym : IModel
+    {
+        public delegate void Callback(Pokemon winner, Pokemon loser);
+        private Pokemon My_Battle_Pokemon;
+        private Pokemon Enemy_Pokemon;
+        public Battle_Gym(Pokemon me, Pokemon enemy)
+        {
+            My_Battle_Pokemon = me;
+            Enemy_Pokemon = enemy;
+        }
+        public void Attack(Pokemon src, Pokemon target, Callback callback)
+        {
+            // transfer each attack into deduction of health, and callback to the presenter if one pokemon "die"
+            if (target.Deduct_Health(src.Damage))
+            {
+                callback(src, target);
+            }
+        }
+    }
+}
---
+++ Pokemon_Go_3080/Pokemon_Go_3080/Presenter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemon_Go_3080
+
+{
+    class GUI_Presenter
+    {
+        private readonly IView View;
+        public GUI_Presenter(IView View)
+        {
+            this.View = View;
+        }
+    }
+}
